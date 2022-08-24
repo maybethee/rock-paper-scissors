df6563f64886ced343b3f41d06aa7a6f3fd1b348
maybethee <jozalamun@gmail.com>
2022-08-24 18:55:53 -0400

    Edit the playRound function to take user input and create new 5-round game function
    I edited the logic in the playRound function to accept a user's input
    and determine whether or not the input beat the computer's random choice,
    as well as storing a win/loss tally for the round.
    
    the 5-round game function loops the playRound function and displays
    the total scores for the player and the computer at the end of the game.

--- rps.js
+++ rps.js
@@ -1,54 +1,83 @@
-//   
-//    take the string values and determine which is the winner (??how??)
-//       write out all possibilites as if statements? if (rock, scissors || scissors, paper || paper, rock) then you win, $playerSelection beats $computerSelection etc.
-// 
-//    ensure the user's input is case-insensitive
-// take these two values and evaluate which side is the winner based on canonical rock paper scissors rules
-// display a message that declares the winner of the round ("You Lose! Paper beats Rock")
-// save (return) the value of the results
-// create a new 5-round version of the game
-// after each round, report which side won and lost, and record this information to determine which side is the winner and loser at the end
-// loop to the next round until 5 rounds are over
-// display who won with how many points
+// array holds possible RPS options
+const choiceOptions = ['ROCK', 'PAPER', 'SCISSORS'];
 
-
-
-
-
-const choiceOptions = ['Rock', 'Paper', 'Scissors'];
 // get a random selection from the computer, either Rock Paper or Scissors. 
 function getComputerChoice() {
+  // pick random string from the length of the choiceOptions array
   const randomChoice = choiceOptions[Math.floor(Math.random() * choiceOptions.length)];
   return randomChoice;
 }
 
-
-const playerSelection = 'rock';
-const computerSelection = 0;
-// logging the function getComputerChoice() mutliple tims gives random selection each time, but when calling the variable computer selection (which i assumed called the function whenever _it_ was called), the selection doesn't change after each call.
-
-
-
+// initialize computer and player selections
+const computerSelection = '';
+const playerSelection = '';
+// initialize win/loss counts
+let winCount = 0;
+let loseCount = 0;
 
 // function that simulates a game with the random computer choice and returns the win/loss result
 function playRound(playerSelection, computerSelection) {
+  // give computerSelection the computerChoice value for the round
   computerSelection = getComputerChoice();
-  console.log(computerSelection);
-  if (computerSelection === 'Scissors') {
-    return `You win! Rock beats ${computerSelection}`;
-  }
-  else if (computerSelection === 'Rock') {
-    return 'It\'s a tie! play again?';
-  }
-  else {
-    return `You lose! ${computerSelection} beats Rock`;
+  // prompt for getting player's selection, making the prompt case insensitive
+  playerSelection = prompt('Rock Paper Scissors').toUpperCase();
+  // display both selections
+  console.log(`You chose: ${playerSelection}`);
+  console.log(`Computer chose: ${computerSelection}`);
+  const winMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+  const loseMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
+  const tieMessage = 'It\'s a tie!';
+  // if statement block to determine the winner
+  if (computerSelection === playerSelection) {
+    console.log(tieMessage);
+    return tieMessage;
+  } else if (computerSelection === 'ROCK' && playerSelection === 'PAPER' ||
+              computerSelection === 'PAPER' && playerSelection === 'SCISSORS' ||
+              computerSelection === 'SCISSORS' && playerSelection === 'ROCK') {
+    console.log(winMessage);
+    return winCount++;
+  } else if (computerSelection === 'ROCK' && playerSelection === 'SCISSORS' ||
+              computerSelection === 'PAPER' && playerSelection === 'ROCK' ||
+              computerSelection === 'SCISSORS' && playerSelection === 'PAPER') {
+    console.log(loseMessage);
+    return loseCount++;
+  } else {
+    // i want to make a cancel message here but idk how
+    return;
   }
 }
-//test to see that computer uses the random choice each time playRound is executed in the console
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
 
+//Five round function called game()
+function game() {
+// play the playRound function 5 times.
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+  }
+  // tell the player the results
+  console.log('Game Over');
+  console.log(`Your score: ${winCount}`);
+  console.log(`Computer score: ${loseCount}`);
+}
+
+game();
+
+// code graveyard:
+
+// note: what seems to be wrong when i log this function, is that it takes the first if statement to be true every time, which means i still can't figure out how to properly utilize the returned value of the playRound function as a boolean to make the if statements work as i want.
+
+// inside the above for loop:
+  // if ('It\'s a tie! play again?') {
+  // // redo iteration
+  //   console.log('tie');
+  // } else if (`You win! Rock beats ${computerSelection}`) {
+  //   // add point to winCount and iterate
+  //   console.log('win');
+  //   winCount = winCount + 1;
+  // } else if (`You lose! ${computerSelection} beats Rock`) {
+  //   // add point to loseCount and iterate
+  //   console.log('lose');
+  //   loseCount = loseCount + 1;
+  // } else {
+  //   console.log('Game over!');
+  // }
+  // }
